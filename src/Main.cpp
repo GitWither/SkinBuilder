c23c1c6e0df308efa498bdf4d7a4d5df96f4b94b
Daniel <wither.tweets@gmail.com>
2022-08-20 16:48:05 +0200

    Added and linked glfw, glm, Vulkan dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include "glm/vec2.hpp"
+#include <vulkan/vulkan.h>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Skin Builder", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
